ec99c196053535111ba0f27c9bd5b9396d1ce70e
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-04 17:36:29 +0600

    post method form submitted

--- django_form/first_app/views.py
+++ django_form/first_app/views.py
@@ -5,8 +5,19 @@ def home(request):
     return render(request,'home.html')
 
 def about(request):
-    return render(request,'about.html')
+    if request.method == 'POST':
+        name=request.POST.get('username')
+        email=request.POST.get('email')
+        select=request.POST.get('select')
+        return render(request,'about.html',{'name': name,'email': email,'select': select})
+    else:
+      return render(request,'about.html')
 
 def submit_form(request):
-    print(request.POST)
+    # if request.method == 'POST':
+    #     name=request.POST.get('username')
+    #     email=request.POST.get('email')
+    #     return render(request,'form.html',{'name': name,'email': email})
+    # else:
+    #     return render(request,'form.html')
     return render(request,'form.html')
