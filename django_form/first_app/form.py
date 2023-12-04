63b0cebb69cfed75fc8add906d5c57133ab0264e
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-05 00:29:09 +0600

    oploaded image by form

--- django_form/first_app/form.py
+++ django_form/first_app/form.py
@@ -2,5 +2,14 @@ from django import forms
 
 
 class contact_form(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
+    name = forms.CharField(label='User Name')
+    file = forms.FileField()
+    # email = forms.EmailField()
+    # age=forms.IntegerField()
+    # weight= forms.FloatField()
+    # balance=forms.DecimalField()
+    # check = forms.BooleanField()
+    # CHOICE= [('S','SMALL'),('M','MEDIUM'),('L','LARGE')]
+    # size= forms.ChoiceField(choices=CHOICE)
+    # MEAL=[('V','VAT'),('M','MANGSHO'),('D','DAL')]
+    # khabar= forms.MultipleChoiceField(choices=MEAL)
--- django_form/first_app/views.py
+++ django_form/first_app/views.py
@@ -24,5 +24,15 @@ def submit_form(request):
     return render(request,'form.html')
 
 def django_form(request):
-    form=contact_form()
+    if request.method == 'POST':
+     form=contact_form(request.POST,request.FILES)
+     if form.is_valid():
+        file= form.cleaned_data['file']
+        # print(form.cleaned_data)
+        with open('./first_app/upload/'+ file.name, 'wb+') as destination:
+           for chunk in file.chunks():
+              destination.write(chunk)
+        return render(request,'django_form.html',{'form': form})
+    else:
+       form=contact_form()
     return render(request,'django_form.html',{'form': form})
