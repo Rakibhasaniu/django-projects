612ffe3eb7925d8c2dfadd5e9c8816a9c972a57a
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-05 18:56:29 +0600

    create super user and explore admin panel

--- modelform_django/first_app/models.py
+++ modelform_django/first_app/models.py
@@ -6,3 +6,7 @@ class Student(models.Model):
     roll = models.IntegerField(primary_key= True)
     address = models.TextField()
     father_name = models.TextField(default='razzak')
+
+    def __str__(self):
+        return self.name
+    
