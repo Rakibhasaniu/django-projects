1dce311ca4837c6ce953fea4091d06584087d30a
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-06 01:01:03 +0600

    creating model realtionship

---
+++ blog_django/author/urls.py
@@ -0,0 +1,8 @@
+
+
+from django.urls import path , include
+
+urlpatterns = [
+    
+    path('add/',views.add_author, name='add_author')
+]
---
+++ blog_django/profiles/urls.py
@@ -0,0 +1,8 @@
+
+
+from django.urls import path , include
+
+urlpatterns = [
+    
+    path('add /',views.add_profile, name='add_profile')
+]
