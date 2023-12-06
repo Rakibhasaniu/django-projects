ead6d21265c7fa8f34b1166418d9c063ce2a7c9c
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-06 20:19:07 +0600

    author data send backend from form

--- blog_django/profiles/views.py
+++ blog_django/profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def add_profile(request):
+    if request.method == 'POST':
+        author_form = forms.AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('add_author')
+    else:
+        author_form = forms.AuthorForm()
+    return render(request,'add_profile.html')
