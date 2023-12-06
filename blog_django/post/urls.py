78dace68b758b050da453d85121a9df486446654
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-07 00:13:03 +0600

    edit post implemented

--- blog_django/post/urls.py
+++ blog_django/post/urls.py
@@ -4,5 +4,6 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     
-    path('add/',views.add_post, name='add_post')
+    path('add/',views.add_post, name='add_post'),
+    path('edit/<int:id>',views.edit_post, name='edit_post')
 ]
--- blog_django/post/views.py
+++ blog_django/post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
+from post.models import Post
 # Create your views here.
 
 def add_post(request):
@@ -11,4 +12,20 @@ def add_post(request):
     
     else: # user normally website e gele blank form pabe
         post_form = forms.PostForm()
+    return render(request, 'add_post.html', {'form' : post_form})
+
+
+
+
+def edit_post(request,id):
+    post = Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    if request.method == 'POST': 
+        post_form = forms.PostForm(request.POST, instance=post) 
+        if post_form.is_valid(): 
+            post_form.save() 
+            return redirect('homepage') 
+    
+    # else: 
+    #     post_form = forms.PostForm()
     return render(request, 'add_post.html', {'form' : post_form})
