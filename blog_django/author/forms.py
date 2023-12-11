451a00f6ccfcae0d6a0475b7089edbd1b6ede98a
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-11 16:42:04 +0600

    delete profile

--- blog_django/author/forms.py
+++ blog_django/author/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Author
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class AuthorForm(forms.ModelForm):
+# class AuthorForm(forms.ModelForm):
+#     class Meta:
+#         model= Author
+#         fields = '__all__'
+
+class RegistrationForm(UserCreationForm):
+    first_name= forms.CharField(required=True)
+    last_name= forms.CharField(required=True)
     class Meta:
-        model= Author
-        fields = '__all__'
+        model = User
+        fields=['username','first_name','last_name','email']
