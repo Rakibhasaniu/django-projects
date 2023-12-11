9b8968380e860e98f668458ad2ff724aac68e491
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-12 00:22:36 +0600

    login successfully

--- blog_django/author/urls.py
+++ blog_django/author/urls.py
@@ -4,5 +4,6 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     
-    path('register/',views.register, name='register')
+    path('register/',views.register, name='register'),
+    path('login/',views.user_login, name='user_login')
 ]
--- blog_django/author/views.py
+++ blog_django/author/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , redirect
 from . import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 # Create your views here.
 # def add_author(request):
 #     if request.method == 'POST':
@@ -19,10 +21,26 @@ def register(request):
             register_form = forms.RegistrationForm(request.POST)
             if register_form.is_valid():
                 register_form.save()
+                messages.success(request,'Account created successfully')
                 return redirect('register')
         else:
             register_form = forms.RegistrationForm()
         return render(request,'register.html',
-        {'form': register_form})
+        {'form': register_form, 'type': 'Register'})
 def user_login(request):
-      if request.method == 'POST'
+      if request.method == 'POST':
+           form = AuthenticationForm(request,request.POST)
+           if form.is_valid():
+                user_name = form.cleaned_data['username']
+                user_pass = form.cleaned_data['password']
+                user = authenticate(username=user_name,password=user_pass)
+                if user is not None:
+                     messages.success(request,'Account login successfully')
+                     login(request,user)
+                     return redirect('register')
+                else:
+                     messages.warning(request,'Login Denied')
+                     return redirect('register')
+      else:
+           form = AuthenticationForm()
+           return render(request,'register.html',{'form': form ,'type': 'Login'})
