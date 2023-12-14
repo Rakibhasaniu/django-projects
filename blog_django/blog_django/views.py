6cadd7ac9fb885b7bbe5cd672b41fadd7598c37f
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-15 00:09:39 +0600

    filter post by category completed

--- blog_django/blog_django/views.py
+++ blog_django/blog_django/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from post.models import Post
-def home(request):
+from categories.models import Category
+def home(request, category_slug = None):
+
     data = Post.objects.all()
-    return render(request,'home.html',{'data': data})
+    if category_slug is not None:
+        category = Category.objects.get( slug = category_slug)
+        data = Post.objects.filter(category = category)
+    category = Category.objects.all()
+    return render(request,'home.html',{'data': data, 'category': category})
