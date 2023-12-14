e7c167880e089067eddbce7ab39647f60307bdd0
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-14 21:43:21 +0600

    logout implemented

--- blog_django/post/views.py
+++ blog_django/post/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from . import forms
 from post.models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def add_post(request):
     if request.method == 'POST': # user post request koreche
         post_form = forms.PostForm(request.POST) # user er post request data ekhane capture korlam
@@ -18,13 +20,14 @@ def add_post(request):
 
 
 
-
+@login_required
 def edit_post(request,id):
     post = Post.objects.get(pk=id)
     post_form = forms.PostForm(instance=post)
     if request.method == 'POST': 
         post_form = forms.PostForm(request.POST, instance=post) 
         if post_form.is_valid(): 
+            post_form.instance.author = request.user
             post_form.save() 
             return redirect('homepage') 
     
@@ -32,7 +35,7 @@ def edit_post(request,id):
     #     post_form = forms.PostForm()
     return render(request, 'add_post.html', {'form' : post_form})
 
-
+@login_required
 def delete_post(request, id):
     post = Post.objects.get(pk=id)
     post.delete()
